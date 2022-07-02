8ec34ca5cc339dd78169831d4bac2a17fad3b239
Francisco Hernández <mihaelkyeah@gmail.com>
2022-07-01 21:55:13 -0300

    Agregado producto_pagina

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -19,7 +19,6 @@ function iniciarPagina() {
     document.getElementById("pag3").addEventListener('click', (e) => {
         cargar_pagina(url_client);
     });
-
     document.getElementById("pag4").addEventListener('click', (e) => {
         cargar_pagina(url_cart);
     });
@@ -41,7 +40,7 @@ function iniciarPagina() {
                 client_pagina();
             }
             if (pagina === url_productos) {
-
+                product_pagina();
             }
             if (pagina === url_cart) {
 
@@ -56,13 +55,13 @@ function iniciarPagina() {
         function client_pagina() {
             const url = 'http://localhost:8080/client';
 
-                cargarTabla();
-                document.querySelector('#client-submit').addEventListener('click', (event) => {
-                    event.preventDefault();
-                    let elem = armarElemento();
-                    console.log(elem);
-                    agregarServidor(elem);
-                });
+            cargarTabla();
+            document.querySelector('#client-submit').addEventListener('click', (event) => {
+                event.preventDefault();
+                let elem = armarElemento();
+                console.log(elem);
+                agregarServidor(elem);
+            });
 
             /**
              * Obtiene los datos de la url y la carga en la tabla
@@ -89,7 +88,7 @@ function iniciarPagina() {
                         }
 
                         elementos.forEach(function(elemento){
-                            cargar(elemento, num);
+                            cargar(elemento);
                         });
                     })
                     .catch(function(error) {
@@ -244,7 +243,7 @@ function iniciarPagina() {
             function guardarCambios(fila, valoresAnteriores, id)
             {
                 //Deja de poder se editable
-                let  children = Array.from(document.getElementById(fila.id).children);
+                let children = Array.from(fila.children);
                 let i = 0;
                 children.forEach(child => {
                     if (child.tagName !== "BUTTON")
@@ -279,7 +278,7 @@ function iniciarPagina() {
             {
                 //Se guardan los valores viejos y las celdas se vuelven editables
                 let valoresAnteriores  = [];
-                let  children = Array.from(document.getElementById(fila.id).children);
+                let children = Array.from(fila.children);
                 let i = 0;
 
                 children.forEach(child => {
@@ -299,7 +298,7 @@ function iniciarPagina() {
             }
 
             function volverValoresAnteriores(fila, valoresAnteriores){
-                let  children = Array.from(document.getElementById(fila.id).children);
+                let children = Array.from(fila.children);
                 let i = 0;
                 children.forEach(child => {
                     if (child.tagName !== "BUTTON")
@@ -324,8 +323,277 @@ function iniciarPagina() {
                 return elemento;
             }
         }
+
+        function product_pagina() {
+            const url = 'http://localhost:8080/product';
+          
+            cargarTabla();
+            document.querySelector('#product-submit').addEventListener('click', (event) => {
+                event.preventDefault();
+                let elem = armarElemento();
+                console.log(elem);
+                agregarServidor(elem);
+            });
+          
+            /**
+             * Obtiene los datos de la url y la carga en la tabla
+             */
+            function cargarTabla(){
+                fetch(url + "/all", {
+                    method: 'GET',
+                    mode: 'cors',
+                })
+                    .then(function(respuesta){
+                        if(respuesta.ok) {
+                            return respuesta.json();
+          
+                        }
+                        else {
+                            alert("Error");
+                        }
+                    })
+                    .then(function(elementos){
+                        let tabla = document.getElementById("product-table");
+                        while ((tabla.rows.length - 1) > 0)
+                        {
+                            tabla.deleteRow(-1);
+                        }
+          
+                        elementos.forEach(function(elemento){
+                            cargar(elemento);
+                        });
+                    })
+                    .catch(function(error) {
+                        console.log("Hubo un problema con la petición Fetch: " + error.message);
+                    });
+            }
+          
+            /**
+             * @param {elemento}
+             * Crea una nueva fila en la tabla
+             */
+            function cargar(elemento){
+          
+                console.log(elemento);
+                let tabla = document.getElementById("product-table");
+                let fila = tabla.insertRow(-1);
+                //Editar por id de tabla
+                let id = elemento.idProduct;
+                fila.setAttribute("id","fila-".id);
+                //elemento.delete(IdProduct);
+                delete elemento.idProduct;
+                console.log(elemento);
+                //Crea todos los elementos
+                for(var key in elemento){
+                    let cel = fila.insertCell(0);
+                    cel.innerHTML = elemento[key];
+                    //fila.appendChild(document.createElement("td").text(elemento[key]));
+                }
+                //Crea el boton de editar
+                let btnEditar = document.createElement("button");
+                btnEditar.innerHTML = "Editar";
+                btnEditar.type = "button";
+          
+                btnEditar.addEventListener('click', function(){
+                    editarElemento(fila, id);
+                });
+                fila.appendChild(btnEditar);
+          
+                //Crea el boton de borrar
+                let btnBorrar = document.createElement("button");
+                btnBorrar.innerHTML = "Borrar";
+                btnBorrar.type = "button";
+                btnBorrar.addEventListener('click', function(){
+                    borrarElemento(fila, id);
+                });
+                fila.appendChild(btnBorrar);
+                fila.setAttribute("id", "fila-".id);
+                // Agrega la fila a la tabla
+                tabla.appendChild(fila);
+            }
+          
+            /////////////////////////////  ALTA  /////////////////////////////
+          
+            /**
+             * @returns elemento JSON
+             * Devuelve un json cargados con los valores leidos del formulario
+             */
+            function armarElemento()
+            {
+                let elemento = {
+                    "name": document.getElementById("product-name").value,
+                    "description": document.getElementById("product-description").value,
+                    "price": document.getElementById("product-price").value
+                }
+                return elemento;
+            }
+          
+            /**
+             *
+             * @param {*} elemento
+             * Agrega un elemento al servidor
+             */
+            function agregarServidor (elemento){
+                // Escribe el objeto en el JSON del servidor
+                console.log(elemento);
+                fetch((url + "/add"), {
+                    'method': 'POST',
+                    'headers': {
+                        'content-type': 'application/JSON'
+                    },
+                    'mode': 'cors',
+                    'body': JSON.stringify(elemento)
+                })
+                    .then(function (respuesta) {
+                        if (respuesta.ok) {
+                            cargarTabla(url);
+                        }
+                        else {
+                            alert("La solicitud al servidor falló.");
+                        }
+                    })
+                    .catch(function(error) {
+                        console.log("Hubo un problema con la petición Fetch:" + error.message);
+                    });
+            }
+          
+            /**
+             *
+             * @param {fila, id}
+             * Elimina un elemento del servidor y de la tabla en el html
+             */
+            function borrarElemento(fila, id)
+            {
+                if (confirm("¿Seguro que desea eliminarlo?"))
+                {
+                    fetch((url + '/deleteByID/' +id),{
+                        'method':'DELETE',
+                        'mode': 'cors'
+                    })
+                        .then(function(respuesta){
+                            if(respuesta.ok) {
+                                let tabla = document.getElementById("product-table");
+                                tabla.removeChild(fila);
+                            }
+                            else {
+                                alert("No se pudo eliminar");
+                            }
+                        });
+                }
+            }
+          
+          
+            //Esta funcion edita un pedido del servidor, de no poder hacerlo restaura los valores viejos en la tabla
+            function editarServidor(fila, id, valoresAnteriores)
+            {
+                //Crea el pedido los valores de la fila alterada
+                let elemento = leerFila(fila);
+                //Encuentra el pedido por su ID y lo pisa con los datos nuevos
+                console.log(elemento);
+                fetch((url+ '/update/' +id),{
+                    'method':'PUT',
+                    'mode': 'cors',
+                    'headers': {
+                        'content-type': 'application/JSON'
+                    },
+                    'body': JSON.stringify(elemento)
+                })
+                    .then(function(respuesta){
+                        if(!respuesta.ok) {
+                            //Si falla el PUT restaura los valores anteriores en la tabla
+                            volverValoresAnteriores(fila, valoresAnteriores)
+                            alert("No se pudo modificar el pedido");
+                        }
+                        else {
+                            //Si funciona el PUT
+                            alert("Datos modificados");
+                        }
+                    })
+            }
+          
+            //Esta funcion deja de permitir la edicion de la tabla y que cambios ocurren
+            function guardarCambios(fila, valoresAnteriores, id)
+            {
+                //Deja de poder se editable
+                let children = Array.from(fila.children);
+                let i = 0;
+                children.forEach(child => {
+                    if (child.tagName !== "BUTTON")
+                    {child.contentEditable = "false";
+                    i++;}
+                });
+                //Verifica si esta todo bien
+                if (cambioValido())
+                {
+                    //Pregunta si esta seguro
+                    if (confirm("¿Seguro que desea modificar?"))
+                    {
+                        editarServidor(fila, id, valoresAnteriores);
+                    }
+                    else
+                    {
+                        volverValoresAnteriores(fila, valoresAnteriores);
+                    }
+                }
+                else {volverValoresAnteriores(fila, valoresAnteriores);}
+                //Vuelve a poner el mismo boton que estaba antes, esto se hizo porque si solo modificabas el evento bucleaba
+                let btnEditar = document.createElement("button");
+                btnEditar.innerHTML = "Editar";
+                btnEditar.type = "button";
+                btnEditar.addEventListener('click', function(){editarPedido(fila, id);});
+                fila.replaceChild(btnEditar,fila.children[i]);
+          
+            }
+          
+            //Esta funcion permite la edicion de la tabla
+            function editarElemento(fila, id)
+            {
+                //Se guardan los valores viejos y las celdas se vuelven editables
+                let valoresAnteriores  = [];
+                let children = Array.from(fila.children);
+                let i = 0;
+          
+                children.forEach(child => {
+                    console.log(child);
+                    if (child.tagName !== "BUTTON")
+                    {
+                        valoresAnteriores[i]= child.innerHTML;
+                        child.contentEditable = "true";
+                        i++;}
+                });
+                //Se crea un boton que al apretarse confirma los cambios, este reemplaza el boton anterior
+                let btnGuardar = document.createElement("button");
+                btnGuardar.innerHTML = "Guardar";
+                btnGuardar.type = "button";
+                btnGuardar.addEventListener('click', function(){guardarCambios(fila, valoresAnteriores, id);});
+                fila.replaceChild(btnGuardar,fila.children[i]);
+            }
+          
+            function volverValoresAnteriores(fila, valoresAnteriores){
+                let children = Array.from(fila.children);
+                let i = 0;
+                children.forEach(child => {
+                    if (child.tagName !== "BUTTON")
+                    {
+                        child.innerHTML = valoresAnteriores[i];
+                        i++;
+                    }
+                });
+            }
+          
+            function cambioValido(){
+          
+                return true;
+            }
+          
+            function leerFila(fila){
+                let elemento = {
+                    "name": fila.children[2].innerHTML,
+                    "description": fila.children[1].innerHTML,
+                    "price": fila.children[0].innerHTML
+                }
+                return elemento;
+            }
         }
-
-
-
+    }
 }
